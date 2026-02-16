b1e011fd25a739698c2f9c4ea5758632677d923d
Rim Gammoudi <rimgamoudi111@gmail.com>
2026-02-16 04:22:23 +0100

    adding globalBudget Code

--- src/main/java/clientApisCall/GlobalBudgetApis/GlobalBudgetApis.java
+++ src/main/java/clientApisCall/GlobalBudgetApis/GlobalBudgetApis.java
@@ -1,2 +1,30 @@
-package clientApisCall.GlobalBudgetApis;public class GlobalBudgetApis {
+package clientApisCall.GlobalBudgetApis;
+
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+
+import static genericApi.GenericApiCall.createAnObject;
+import static genericApi.GenericApiCall.getTheObject;
+
+public class GlobalBudgetApis {
+    /**
+     * @Author Rim Gammoudi
+     */
+
+
+    //Method useful to create an global Budget
+    public static Response createAglobalBudget(Object payload, String endPoint) {
+        return createAnObject(payload, endPoint);
+    }
+
+    //Method useful to eextract a list of global budget with criteria
+
+    public static Response extractAlistOfBudget(HashMap<String, Object> queryParam, String endPoint) {
+
+        return getTheObject(queryParam, endPoint);
+    }
+
+
+
 }
--- src/test/java/ApisTest/globalBudgetTests/GlobalBudgetTests.java
+++ src/test/java/ApisTest/globalBudgetTests/GlobalBudgetTests.java
@@ -1,2 +1,49 @@
-package ApisTest.globalBudgetTests;public class GlobalBudgetTests {
+package ApisTest.globalBudgetTests;
+
+
+import configManager.ConfigurationReader;
+import io.restassured.response.Response;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+import pojos.globalBudget.globalBudgetRequestPojo;
+
+import java.util.HashMap;
+
+import static ApisTest.inventoryTests.InventoryTests.pathOfFolderProperty;
+import static asserts.Asserts.isResponseStatusCorrect;
+import static clientApisCall.GlobalBudgetApis.GlobalBudgetApis.createAglobalBudget;
+import static clientApisCall.GlobalBudgetApis.GlobalBudgetApis.extractAlistOfBudget;
+
+@Slf4j
+public class GlobalBudgetTests {
+    private final String globalBudgetProperty = "globalBudget";
+    private final String globalBudgetPeriodProperty = "globalBudgetPeriod";
+
+    //Method useful to  create a globalBudgetTest
+    @SneakyThrows
+    @Test()
+    public void createGlobalBudgetTestShouldBeSuccessful() {
+        globalBudgetRequestPojo globalBudget = globalBudgetRequestPojo.builder()
+                .month(2)
+                .year(2026)
+                .value(15).build();
+        Response res = createAglobalBudget(globalBudget, ConfigurationReader.getProperty(pathOfFolderProperty) + ConfigurationReader.getProperty(globalBudgetProperty));
+        System.out.println(res.body().asPrettyString());
+        isResponseStatusCorrect(res, 200);
+    }
+
+    //Method useful to  create a globalBudgetTest
+    @SneakyThrows
+    @Test()
+    public void GlobalBudgetTestShouldReturnTheCorrectList() {
+        HashMap<String, Object> globalBudget = new HashMap<>();
+        globalBudget.put("from", "14-01-2026");
+        globalBudget.put("to", "16-02-2026");
+        Response res = extractAlistOfBudget(globalBudget, ConfigurationReader.getProperty(pathOfFolderProperty) + ConfigurationReader.getProperty(globalBudgetProperty) + ConfigurationReader.getProperty(globalBudgetPeriodProperty));
+        System.out.println(res.body().asPrettyString());
+        isResponseStatusCorrect(res, 200);
+    }
+
+
 }
